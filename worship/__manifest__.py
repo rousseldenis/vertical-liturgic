0fc3e992ea7dd94784b864c64ab6f09a1dc5fe75
Denis Roussel <denis.roussel@acsone.eu>
2019-08-06 11:30:16 +0200

    [12.0][ADD] worship

---
+++ worship/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Denis Roussel
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Worship',
+    'summary': """
+        Adds base application for Worship management""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Denis Roussel,Odoo Community Association (OCA)',
+    "application": True,
+    'depends': [
+        "base",
+    ],
+    'data': [
+        "views/menu.xml",
+        "security/security.xml",
+    ],
+    'demo': [
+    ],
+}
