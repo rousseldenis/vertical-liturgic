ade7270b0b67c9e0e193b502b2455642031abf9c
Denis Roussel <denis.roussel@acsone.eu>
2020-10-14 08:46:01 +0200

    [14.0][MIG] worship*

--- worship_event/__manifest__.py
+++ worship_event/__manifest__.py
@@ -2,19 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Worship Event',
-    'summary': """
+    "name": "Worship Event",
+    "summary": """
         Adds worship event management""",
-    'version': '13.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Denis Roussel,Odoo Community Association (OCA)',
-    'depends': [
-        "worship",
-        "event"
-    ],
-    'data': [
-        'views/event_event.xml',
-    ],
-    'demo': [
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Denis Roussel,Odoo Community Association (OCA)",
+    "depends": ["worship", "event"],
+    "data": [
+        "views/event_event.xml",
     ],
 }
