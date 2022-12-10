b77889502506b8f660833ecee2260676579ae7a0
Denis Roussel <denis.roussel@acsone.eu>
2022-12-10 11:47:44 +0100

    [IMP] worship_event: pre-commit stuff

--- worship_event/models/event_event.py
+++ worship_event/models/event_event.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Denis Roussel
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import models
 
 
 class EventEvent(models.Model):
--- worship_event/models/event_type.py
+++ worship_event/models/event_type.py
@@ -1,10 +1,9 @@
 # Copyright 2019 Denis Roussel
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import models
 
 
 class EventType(models.Model):
 
-    _inherit = 'event.type'
-
+    _inherit = "event.type"
