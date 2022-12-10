399dc9d6d348c4fdcbdc823ac084fdb007c74119
Denis Roussel <denis.roussel@acsone.eu>
2022-12-10 11:51:39 +0100

    [MIG][16.0] worship_event

--- worship_event/__manifest__.py
+++ worship_event/__manifest__.py
@@ -1,12 +1,13 @@
 # Copyright 2019 Denis Roussel
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Worship Event",
     "summary": """
         Adds worship event management""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
+    "website": "https://github.com/OCA/vertical-worship",
     "author": "Denis Roussel,Odoo Community Association (OCA)",
     "depends": ["worship", "event"],
     "data": [
